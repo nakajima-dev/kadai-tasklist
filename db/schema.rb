bd29123590975b58bc4338f9c02e6f2e4c1f2f05
yukito.nakajima <yukito.nakajima@ntt.com>
2020-05-19 05:02:51 +0000

    add colmun

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_010120) do
+ActiveRecord::Schema.define(version: 2020_05_19_043937) do
 
   create_table "task_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_010120) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
